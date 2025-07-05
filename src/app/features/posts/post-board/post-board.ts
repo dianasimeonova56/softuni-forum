deb27b2d0f643276671468eadf80f630c8f0cd10
dianasim <diana.simeonova56@gmail.com>
2025-07-05 19:14:06 +0300

    add items to theme/post boards

--- src/app/features/posts/post-board/post-board.ts
+++ src/app/features/posts/post-board/post-board.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../../../core/services';
 import { Post } from '../../../models';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { PostItem } from '../post-item/post-item';
 
 @Component({
   selector: 'app-post-board',
-  imports: [],
+  imports: [PostItem],
   templateUrl: './post-board.html',
   styleUrl: './post-board.css'
 })
--- src/app/features/themes/theme-board/theme-board.ts
+++ src/app/features/themes/theme-board/theme-board.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ThemesService } from '../../../core/services';
 import { Theme } from '../../../models';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import { ThemeItem } from "../theme-item/theme-item";
 
 @Component({
   selector: 'app-theme-board',
-  imports: [],
+  imports: [ThemeItem],
   templateUrl: './theme-board.html',
   styleUrl: './theme-board.css'
 })
