bd17b45e7c5804f514b83aa683f99d20b1a554d0
dianasim <diana.simeonova56@gmail.com>
2025-07-21 23:21:04 +0300

    add auth service (dummy data no real server interaction)

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,72 @@
+import { Injectable, signal } from "@angular/core";
+import { User } from "../../models";
+
+@Injectable({
+    providedIn: 'root' // we can inject it in the project] we can create instance of it 
+    //we provide it in the root of the project to be available everywhere?
+})
+
+export class AuthService {
+    private _isLoggedIn = signal<boolean>(false);
+    private _currentUser = signal<User | null>(null);
+    private _users: User[] = [
+        { id: '5fa64a072183ce1728ff3719', username: "David" },
+        { id: '5fa64b162183ce1728ff371d', username: "Johny" },
+        { id: '5fa64b972183ce1728ff3720', username: "Donald" },
+    ]
+
+    constructor() {
+        const savedUser = localStorage.getItem('currentUser') // check if user exists
+        if (savedUser) {
+            const user: User = JSON.parse(savedUser);
+            this._currentUser.set(user);
+            this._isLoggedIn.set(true);
+        }
+    }
+
+
+    login(email: string, password: string): boolean {
+        if (email && password) {
+            const user = this._users[0];
+            this._currentUser.set(user);
+            this._isLoggedIn.set(true);
+
+            localStorage.setItem('currentUser', JSON.stringify(user));
+
+            return true;
+        }
+
+        return false;
+    }
+
+    register(username: string, email: string, password: string, rePassword: string): boolean {
+        if (username && email && password && rePassword) {
+            if (rePassword === password) {
+                const newUser: User = {
+                    id: `user_${Date.now}`,
+                    username: username
+                }
+                this._users.push(newUser);
+
+                this._currentUser.set(newUser);
+                this._isLoggedIn.set(true);
+
+                localStorage.setItem('currentUser', JSON.stringify(newUser))
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    logout(): void {
+        this._currentUser.set(null);
+        this._isLoggedIn.set(false);
+
+        localStorage.removeItem('currentUser');
+    }
+
+    getCurrentUserId(): string | null {
+        return this._currentUser()?.id || null
+    }
+}
