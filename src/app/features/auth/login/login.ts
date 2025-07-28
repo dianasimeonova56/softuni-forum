3fb6281a2897e4f7bc256fe8a9468048d5290e24
dianasim <diana.simeonova56@gmail.com>
2025-07-28 21:33:39 +0300

    switch to reactive forms for login

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,71 +1,100 @@
 import { Component, inject, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, EmailValidator, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { AuthService } from '../../../core/services';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login  {
   private authService = inject(AuthService);
   private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
 
-  email: string = '';
-  emailError: boolean = false;
-  password: string = '';
-  passwordError: boolean = false;
-  emailErrorMessage: string = '';
-  passwordErrorMessage: string = '';
+  loginForm: FormGroup;
 
-  validateEmail(): void {
-    if(!this.email) {
-      this.emailError = true;
-      this.emailErrorMessage = 'Email is required';
-    } else if(!this.isValidEmail(this.email)) {
-      this.emailError = true;
-      this.emailErrorMessage = 'Email is not valid';
-    } else {
-      this.emailError = false;
-      this.emailErrorMessage = ''; 
+  constructor() {
+    this.loginForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.pattern(/^(?=.{6,})[a-zA-Z][a-zA-Z0-9._-]*@gmail\.(com|bg)$/)]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    })
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  get isEmailValid(): boolean {
+    return this.email?.invalid && (this.email?.dirty || this.email?.touched) || false;
+  }
+
+  get isPasswordValid(): boolean {
+    return this.password?.invalid && (this.password?.dirty || this.password?.touched) || false;
+  }
+
+  get emailErrorMessage(): string {
+    if (this.email?.errors?.['required']) {
+      return 'Email is required';
     }
-  }
-
-  validatePassword(): void {
-    if (!this.password) {
-      this.passwordError = true;
-      this.passwordErrorMessage = "Password is required";
-    } else if(this.password.length < 4) {
-      this.passwordError = true;
-      this.passwordErrorMessage = "Password must be at least 4 chars";
-    } else {
-      this.passwordError = false;
-      this.passwordErrorMessage = '';
+    if (this.email?.errors?.['pattern']) {
+      return 'Email is not valid';
     }
+    return '';
   }
 
-  isFormValid(): boolean {
-    return Boolean(this.email) && Boolean(this.password) && !this.emailError && !this.passwordError;
+  get passwordErrorMessage(): string {
+    if (this.password?.errors?.['required']) {
+      return 'Password is required';
+    }
+    if (this.password?.errors?.['minlength']) {
+      return 'Password must be at least 4 chars';
+    }
+    return '';
   }
+
 
   onSubmit(): void {
-    this.validateEmail();
-    this.validatePassword();
-
-    if(this.isFormValid()) {
-      const response = this.authService.login(this.email, this.password);
+    if (this.loginForm.valid) {
+      const {email, password} = this.loginForm.value;
+      const response = this.authService.login(email, password);
 
       if (response) {
         this.router.navigate(['/home']);
+      } else {
+        this.markFormGroupTouched();
       }
-    } 
+    }
   }
 
-  private isValidEmail(email: string): boolean {
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    return emailRegex.test(email);
+  private markFormGroupTouched(): void {
+    Object.keys(this.loginForm.controls).forEach(key => {
+      const control = this.loginForm.get(key);
+      control?.markAsTouched();
+    })
   }
+
+  // private isValidEmal(email: string): boolean {
+    
+
+  //   return emailRegex.test(email)
+  // }
+}
+
+export function emailValidator(emailControl: AbstractControl): ValidationErrors | null {
+  const emailRegex = /^(?=.{6,})[a-zA-Z][a-zA-Z0-9._-]*@gmail\.(com|bg)$/;
+  const email = emailControl.value;
+
+  if(email && !emailRegex.test(email)) {
+    return {emailValidator: true}
+  }
+
+  return null;
 }
